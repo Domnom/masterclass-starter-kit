bd49f0bf5f5cfdb81feebb53183ba17001b02744
Dominic Trang <dominic_t@hotmail.co.nz>
2020-09-01 12:07:17 +1200

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+Looks like you're using the masterclass starter kit. Fine choice
---
+++ services/math.ts
@@ -0,0 +1,12 @@
+/**
+ * Returns the sum of the 2 numbers supplied
+ *
+ * @export
+ * @param {number} a
+ * @param {number} b
+ * @returns {number}
+ */
+export function add(a: number, b: number): number
+{
+    return a + b;
+}
